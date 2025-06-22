b77c888f7bb947cb74008dcb76e6a53e79153dc6
tonybnya <nya.tony2010@gmail.com>
2025-06-22 09:35:08 +0100

    chore: remove unused images and update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { ScrollTrigger, SplitText } from "gsap/all";
-import gsap from "gsap";
-
-gsap.registerPlugin(ScrollTrigger, SplitText);
-
 const App = () => {
   return (
-    <div className="flex-center flex-col gap-4 h-[100vh]">
-      <h1 className="text-4xl text-indigo-800">Hello, GSAP!</h1>
-      <p>lorem ipsum</p>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
