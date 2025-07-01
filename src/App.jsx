4813730532b47e587c26202ccb786e843f271576
tonybnya <nya.tony2010@gmail.com>
2025-07-01 11:51:00 +0100

    feat: implement the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
     </main>
   );
 };
